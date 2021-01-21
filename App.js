117c2c603a746c3a8af8083b2283edeff4e3e719
Westonside <weston.t@comcast.net>
2021-01-21 18:15:45 +0000

    initial commit

--- App.js
+++ App.js
@@ -2,20 +2,55 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component{
+  constructor(){
+    super();
+
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return(
+      <View style = {styles.container}>
+        <View style = {styles.statusBar}></View>
+        <Text style = {styles.header}>Welcome</Text>
+        <Text style = {styles.headerDescription}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut faucibus nunc eget risus efficitur auctor. </Text>
+      </View>
+    )
+  }
 }
+
+
+
+// Text Color #2699FB
+// 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+  statusBar:{
+    backgroundColor: '#fff',
+    height: 20
+  },
+  header:{
+    marginTop:40,
+    textAlign:'center',
+    color:'#2699FB',
+    fontSize:40,
+    fontWeight:'bold'
+  },
+  headerDescription:{
+    color:'#2699FB',
+    textAlign:'center',
+    marginLeft:20,
+    justifyContent:'center',
+    marginTop:15,
+    paddingRight: 20,
+    fontSize:14,
+    lineHeight:30,
+  }
 });
